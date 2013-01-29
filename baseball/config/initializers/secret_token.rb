e720ac5a8765048e4e13bca9e670768ae75cda8a
Kriti & Mario <mariozig@gmail.com>
2013-01-29 09:09:02 -0800

    Initial commit

---
+++ baseball/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baseball::Application.config.secret_token = 'a433e1f55428261d3e34faa1b2510984ac05c0a041b90704a2f5c6625b18890f4dd23bdaeb97691a4e8df6c1b872e59866f5ac102c10ee1a83c3712ceb44153a'
